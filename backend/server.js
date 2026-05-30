83877eaa387d02c9f032163d38d51577610d4e44
kerplunkdeveloper-lgtm <kerplunkdeveloper@gmail.com>
2026-05-30 12:03:44 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(
   cors({
     origin: process.env.FRONTEND_URL
       ? process.env.FRONTEND_URL.split(",")
-      : ["http://localhost:5173", "https://boxandcross.com/"],
+      : ["http://localhost:5173", "https://boxandcross.com/", "http://127.0.0.1:5500"],
     credentials: true,
   })
 );
