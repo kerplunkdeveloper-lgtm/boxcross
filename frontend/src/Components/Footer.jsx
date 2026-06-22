e96077fc13359b479a636c67047c1c899a2e245e
kerplunkdeveloper-lgtm <kerplunkdeveloper@gmail.com>
2026-06-22 12:42:11 +0530

    de

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -3,64 +3,65 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="w-full bg-[#111111] border-t border-white/10">
-
       <div
         className="
-          mt-3
+          px-6
           md:px-8
           lg:px-20
-          pt-10
-          pb-28
-          md:py-10
+          py-6
+          md:py-8
           flex
           flex-col
           md:flex-row
           items-center
-          justify-center
-          gap-3
+          justify-between
+          gap-4
           max-w-[1400px]
           mx-auto
         "
       >
-
-        {/* LEFT */}
+        {/* LEFT: Copyright Info */}
         <p
           className="
             text-[#8b8b8b]
             text-center
             md:text-left
+            font-bold
             text-[14px]
-            md:text-[19px]
+            md:text-[16px]
+            leading-relaxed
+            px-16
+            md:px-0
           "
-          style={{ fontFamily: '"Brutal", sans-serif',  }}
+          style={{ fontFamily: '"Brutal", sans-serif' }}
         >
           Copyright © 2026{" "}
-          <span className="text-[#d9ff00] font-bold mx-2">
-           {" "} Box & Cross
-          </span>
-          . All Rights Reserved.
+          <span className="text-[#d9ff00] font-bold mx-1">
+            Box & Cross .
+          </span>{" "}
+          All Rights Reserved.
         </p>
 
-{/* LEFT */}
+        {/* RIGHT: Developer Info */}
         <p
           className="
             text-[#8b8b8b]
             text-center
             md:text-left
+            font-bold
             text-[14px]
-            md:text-[19px]
+            md:text-[16px]
+            px-16
+            md:px-0
           "
-          style={{ fontFamily: '"Brutal", sans-serif',  }}
+          style={{ fontFamily: '"Brutal", sans-serif' }}
         >
-         Developed By
-          <span className="text-[#d9ff00] font-bold mx-2">
-          Kelpunk Media
+          Developed By{" "}
+          <span className="text-[#d9ff00] font-bold ml-1">
+            Kelpunk Media
           </span>
         </p>
-
-
       </div>
-
     </footer>
   );
 };
