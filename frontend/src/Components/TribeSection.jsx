ebf36d70e4b778abf48e83ecf4fe2fe8886838c0
kerplunkdeveloper-lgtm <kerplunkdeveloper@gmail.com>
2026-06-23 14:49:19 +0530

    newupdate

--- frontend/src/Components/TribeSection.jsx
+++ frontend/src/Components/TribeSection.jsx
@@ -32,7 +32,7 @@ export default function TribeSection() {
 
   return (
     <section className="bg-black py-12  sm:px-6 lg:px-8 overflow-hidden">
-      <div className="max-w-7xl mx-auto">
+      <div className="max-w-8xl mx-auto">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-stretch">
 
           {/* LEFT CARD: WHAT THE TRIBE IS */}
