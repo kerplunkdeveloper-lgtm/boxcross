54d4cac14377a50f8ade9f56e9d28175d038dc72
kerplunkdeveloper-lgtm <kerplunkdeveloper@gmail.com>
2026-06-30 16:03:04 +0530

    commit

--- frontend/src/pages/Events/Eventpage.jsx
+++ frontend/src/pages/Events/Eventpage.jsx
@@ -16,14 +16,14 @@ const Eventpage = () => {
         <meta property="og:url" content="https://membership.boxandcross.com/events" />
         <meta property="og:title" content="Events & Class Schedules | Box & Cross" />
         <meta property="og:description" content="View and register for active training sessions, elite gym schedules, and competitive athletic events at Box & Cross." />
-        <meta property="og:image" content={`https://membership.boxandcross.com/events${coverImg}`} />
+        <meta property="og:image" content={`https://membership.boxandcross.com${coverImg}`} />
 
         {/* Twitter */}
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://membership.boxandcross.com/events" />
         <meta property="twitter:title" content="Events & Class Schedules | Box & Cross" />
         <meta property="twitter:description" content="View and register for active training sessions, elite gym schedules, and competitive athletic events at Box & Cross." />
-        <meta property="twitter:image" content={`https://membership.boxandcross.com/events${coverImg}`} />
+        <meta property="twitter:image" content={`https://membership.boxandcross.com${coverImg}`} />
       </Helmet>
       {/*............................ banner section for events page............................*/}
       <Eventbanner/>
